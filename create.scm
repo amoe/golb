3322b4e99a58e8e6119ef232e7a047f62b0a84fb
David Banks <amoebae@gmail.com>
2009-10-23 20:09:13 +0100

    supporting continuation templates

--- create.scm
+++ create.scm
@@ -8,7 +8,6 @@
 (define *create-template-path*         ; For some reason, libxslt wants this to
   "/var/plt-web-server/create.xsl")    ; be absolute.
 
-
 (provide interface-version
          timeout
          start)
@@ -19,9 +18,18 @@
 (define (start req)
   (send/back
    (make-response/full 200 "OK"
-                       (current-seconds) #"text/plain"   ; FIXME: change to real
+                       (current-seconds) #"text/html"   ; FIXME: change to real
                        '()
                        (list (mode:create)))))
 
 (define (mode:create)
-  (format "~s" (xslt:parse-stylesheet-file *create-template-path*)))
+  (let* ((cur (xslt:parse-stylesheet-file *create-template-path*))
+         (xml (k-url->document "foo"))
+         (doc (xslt:parse-memory xml (string-length xml)))
+         (res (xslt:apply-stylesheet cur doc #f)))
+    (let-values (((r1 r2 r3) (xslt:save-result-to-string res cur)))
+      r2)))
+
+(define (k-url->document k-url)
+  (format "<?xml version=\"1.0\"?> <k-url>~a</k-url>"
+          k-url))
---
+++ create.xsl
@@ -0,0 +1,28 @@
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns="http://www.w3.org/TR/xhtml1/strict">
+<xsl:output method="html" indent="yes"/>
+<xsl:template match="/">
+  <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <title>Conforming XHTML 1.1 Template</title>
+    </head>
+    <body>
+      <h1>Create entry</h1>
+      
+      <p>Feel free to type your text below.  You're the boss!</p>
+
+      <p>I will post to: <xsl:value-of select="/k-url"/></p>
+      
+      <form>
+        <xsl:attribute name="action">
+          <xsl:value-of select="/k-url"/>
+        </xsl:attribute>
+        <textarea name="body" rows="25" cols="80"/>
+        <input type="submit" value="Create"/>
+      </form>
+      
+    </body>
+  </html>
+</xsl:template>
+</xsl:stylesheet>
