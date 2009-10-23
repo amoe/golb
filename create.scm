98bb3751aea047d6086fb6c3bb4be60f4f49192e
David Banks <amoebae@gmail.com>
2009-10-23 19:52:37 +0100

    starting to use libxslt

--- create.scm
+++ create.scm
@@ -3,6 +3,12 @@
 (require web-server/http/response-structs)
 (require web-server/servlet/web)
 
+(require (prefix-in xslt: libxslt))
+
+(define *create-template-path*         ; For some reason, libxslt wants this to
+  "/var/plt-web-server/create.xsl")    ; be absolute.
+
+
 (provide interface-version
          timeout
          start)
@@ -11,17 +17,11 @@
 (define timeout 64)
 
 (define (start req)
-  (stage2 
-   (send/suspend
-    (lambda (k-url)
-      (make-response/full 200 "OK"
-                          (current-seconds) #"text/plain"
-                          '()
-                          (list "Hello, world!\n" k-url))))))
-
-(define (stage2 req)
   (send/back
    (make-response/full 200 "OK"
-                       (current-seconds) #"text/plain"
-                      '()
-                      (list "Hello from stage2!\n"))))
+                       (current-seconds) #"text/plain"   ; FIXME: change to real
+                       '()
+                       (list (mode:create)))))
+
+(define (mode:create)
+  (format "~s" (xslt:parse-stylesheet-file *create-template-path*)))
