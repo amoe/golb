681c5037bdd4d45420054a9554720a232ab3ab88
David Banks <amoebae@gmail.com>
2009-10-30 22:22:49 +0000

    configuration infrastructure

---
+++ config.scm
@@ -0,0 +1,9 @@
+#lang scheme
+
+(provide (all-defined-out))
+
+(define *database-host* "localhost")
+(define *database-port* 3306)
+(define *database-user* "golb")
+(define *database-pass* "golb")
+(define *database-name* "golb")
